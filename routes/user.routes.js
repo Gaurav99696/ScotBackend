b6cd6977ca16655427ef26bbb4e983fa0e8bb8c1
gauravMishra <gauravmishra99696@gmail.com>
2024-05-17 20:32:17 +0530

    error fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,6 @@ router.post("/verifyUser", usersControllers.verifyUser)
 router.post("/login", usersControllers.loginUser)
 router.get("/getAllUsers/:userName", usersControllers.getAllUsers)
 router.get("/getSingleUser/:userName", usersControllers.getSingleUser)
-// router.post("/sendMsgToUser", usersControllers.sendMsgToUser)
 router.get("/getMsg/:userName", )
 
 module.exports = router
