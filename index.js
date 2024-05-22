480dfc1453d430bf67a7e9f3517c49031f0ec62e
gauravMishra <gauravmishra99696@gmail.com>
2024-05-22 22:15:12 +0530

    Scoket.io Error fix

--- index.js
+++ index.js
@@ -54,6 +54,73 @@
 //   });
 // });
 
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const initRoutes = require("./routes/index");
+// const { validateUserMsg } = require("./validations/user.validations");
+// const cors = require("cors");
+// const { findUserBy } = require("./servise/user.servise");
+// require("dotenv").config();
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(express.json());
+// app.use(cors());
+// initRoutes(app);
+
+// mongoose
+//   .connect(process.env.MONGO_DB_URI)
+//   .then(() => {
+//     const server = app.listen(port, () => {
+//       console.log(`Server is running on port ${port}`);
+//     });
+//     console.log("Connected! YOU ARE CONNECTED TO YOUR DATABASE!");
+
+//     // Set up Socket.IO server
+//     const io = require("socket.io")(server, {
+//       path: "/chat", // Set the path for Socket.IO
+//       cors: {
+//         origin: ["http://localhost:3000"],
+//       },
+//     });
+
+//     io.on("connection", (socket) => {
+//       console.log("A user connected to /chat");
+
+//       socket.on("sendMSG", async ({ sender, content, reciver }) => {
+//         const data = { content };
+
+//         const { error } = validateUserMsg(data);
+//         if (error) {
+//           socket.emit("error", error.details[0].message);
+//           return;
+//         }
+
+//         const getReciver = await findUserBy("userName", reciver);
+//         const getSender = await findUserBy("userName", sender);
+
+//         getReciver.messages.push({ sender, content });
+//         getSender.messages.push({ sender, content });
+
+//         await getReciver.save();
+//         await getSender.save();
+
+//         socket.broadcast.emit("reciveMSG", { sender, content, reciver });
+//       });
+
+//       socket.on("disconnect", () => {
+//         console.log("A user disconnected from /chat");
+//       });
+//     });
+
+//     io.on("error", (error) => {
+//       console.log(`Socket.IO error: ${error}`);
+//     });
+//   })
+//   .catch(() => console.log("faild! YOU ARE NOT CONNECTED TO YOUR DATABASE!"));
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const initRoutes = require("./routes/index");
@@ -79,14 +146,14 @@ mongoose
 
     // Set up Socket.IO server
     const io = require("socket.io")(server, {
-      path: "/chat", // Set the path for Socket.IO
+      path: '/chat', // Set the path for Socket.IO
       cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000"], // Update this to the origin of your client if different
       },
     });
 
     io.on("connection", (socket) => {
-      console.log("A user connected to /chat");
+      console.log('A user connected to /chat');
 
       socket.on("sendMSG", async ({ sender, content, reciver }) => {
         const data = { content };
@@ -110,12 +177,13 @@ mongoose
       });
 
       socket.on("disconnect", () => {
-        console.log("A user disconnected from /chat");
+        console.log('A user disconnected from /chat');
       });
     });
 
     io.on("error", (error) => {
       console.log(`Socket.IO error: ${error}`);
     });
+
   })
   .catch(() => console.log("faild! YOU ARE NOT CONNECTED TO YOUR DATABASE!"));
